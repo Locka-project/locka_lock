578e0805167606c459404cfd16151c207d90a740
Tony LAUNAY <tony.launay@ynov.com>
2015-10-18 04:17:44 +0200

    Fix some bug and added email authentication

--- app.js
+++ app.js
@@ -2,35 +2,47 @@ var socketIOClient = require('socket.io-client');
 var sailsIOClient = require('sails.io.js');
 var readline = require('readline');
 
-/* Authentication */
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Identifier : ", function(id) {	
-	rl.question("API Key : ", function(api) {	
-		rl.close();
-		connectSocket(id,api)
+/* Functions */
+function loginAPI(){
+	/* Authentication */
+	var rl = readline.createInterface({
+	  input: process.stdin,
+	  output: process.stdout
 	});
-});
+	
+	rl.question('Identifier : ', function(id) {	
+		rl.question('Email : ', function(email) {
+			rl.question('API Key : ', function(api) {	
+				rl.close();
+				connectSocket(id,email,api)
+			});	
+		});
+	});	
+}
 
-function connectSocket(id,api) {
+function connectSocket(id,email,api) {
 	/* Connect Socket IO */
 	var io = sailsIOClient(socketIOClient);
 	io.sails.url = 'http://localhost:1337';
 	
 	io.socket.on('connect', function(){
+		// Listening open event
 		io.socket.on('openDoor', function(data){
-		  console.log('Opening the door.');
+		  console.log('Opening the door...');
 		  console.log(data);
 		});
+		// Listening close event
 		io.socket.on('closeDoor', function(data){
 		  console.log('Closing the door...');
 		  console.log(data);
 		});
-		io.socket.get('/api/device/subscribe/'+id, {access_token: api}, function (data) {
-		  console.log(data)
+		// Subscribe my lock
+		io.socket.get('/api/devices/subscribe/'+id, {access_token: api, email: email}, function (data) {
+			if(data != null){
+				if(data.msg != 'success') {
+					console.log(data);
+				}
+			}
 		});
 		io.socket.on("device", function(data){
 			console.log(data);	
@@ -41,4 +53,6 @@ function connectSocket(id,api) {
 	});
 }
 
+/* Main */
+loginAPI();
 
