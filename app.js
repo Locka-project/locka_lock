a32f59dccd2e69a690a124bdfcdb5c82be185d69
KemCake <santos.remi@icloud.com>
2015-11-20 16:42:10 +0100

    Use Put instead of Post

--- app.js
+++ app.js
@@ -89,14 +89,14 @@ loginAPI();
 function openDoor() {
   console.log('device?', device);
   console.log('Sending post to /api/devices/'+device.id+'/open');
-  io.socket.post('/api/devices/'+device.id+'/open', {access_token: APIKEY}, function (data) {
+  io.socket.put('/api/devices/'+device.id+'/open', {access_token: APIKEY}, function (data) {
     console.log(data);
   });
 }
 
 function closeDoor() {
   console.log('Sending post to /api/devices/'+device.id+'/close');
-  io.socket.post('/api/devices/'+device.id+'/close', {access_token: APIKEY}, function (data) {
+  io.socket.put('/api/devices/'+device.id+'/close', {access_token: APIKEY}, function (data) {
     console.log(data);
   });
 }
